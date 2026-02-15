15024a5917c1af5b6d61ae1c877a793c21f4e050
Ckangel <ckajeigbe1@gmail.com>
2026-02-15 22:13:16 +0000

    storage_service upate

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,44 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class StorageService {
+  static Database? _db;
+
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await _initDB();
+    return _db!;
+  }
+
+  Future<Database> _initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'travel_journal.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE entries(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            note TEXT,
+            photoPath TEXT,
+            latitude REAL,
+            longitude REAL,
+            createdAt TEXT
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertEntry(Map<String, dynamic> entry) async {
+    final db = await database;
+    return await db.insert('entries', entry);
+  }
+
+  Future<List<Map<String, dynamic>>> getEntries() async {
+    final db = await database;
+    return await db.query('entries', orderBy: 'createdAt DESC');
+  }
+}
