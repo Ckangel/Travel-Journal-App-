e3527baa83e052cf851722e6ffae1f4aba2e43ea
Ckangel <ckajeigbe1@gmail.com>
2026-02-15 22:02:09 +0000

    camera_services

---
+++ lib/services/camera_service.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+
+class CameraService {
+  final ImagePicker _picker = ImagePicker();
+
+  Future<File?> takePhoto() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    if (photo != null) {
+      return File(photo.path);
+    }
+    return null;
+  }
+
+  Future<File?> pickFromGallery() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      return File(image.path);
+    }
+    return null;
+  }
+}
